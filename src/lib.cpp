367ceca8480b1ac4163fb66109309f928f13930d
hitimr <hitim@gmail.com>
2021-01-11 18:18:27 +0100

    working implementation of bfs in cpp without mpi

--- src/lib.cpp
+++ src/lib.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -11,9 +13,39 @@ struct oned_csr_graph;
 // global variables from bfs_custom.c
 extern int64_t *column;
 extern int * rowstarts;
+extern int64_t visited_size;
+int64_t *pred_glob;
+
+#define BYTES_PER_VERTEX 6
+#define COLUMN(i) (*(int64_t*)(((char*)column)+(BYTES_PER_VERTEX*i)) & (int64_t)(0xffffffffffffffffULL>>(64-8*BYTES_PER_VERTEX)))
 
 void run_bfs_cpp(int64_t root, int64_t* pred)
 {
-    cout << column[0] << endl;
+    int64_t u,j,v;
+
+    // Initializing the visited array. the predecessor list is cleared before urn_bfs() is called
+    vector<bool> vis(visited_size, false);
+    queue<int64_t> q; // Create empty queue
+
+    q.push(root); // Enter the starting vertex into the queue
+    vis[root] = true;
+    pred[root] = root;
+
+    while(!q.empty())
+    {
+        u = q.front();
+        q.pop();
+
+        for(j = rowstarts[u]; j < rowstarts[u+1]; j++)
+        {
+            v = COLUMN(j);
+            if(!vis[v]) 
+            {
+                vis[v] = true;
+                q.push(v);
+                pred_glob[v] = u;
+            }
+        }
+    }
     return;
 }
