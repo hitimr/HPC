b70eeefcaba642f2558604f0219b8967dba46fee
hitimr <hitim@gmail.com>
2021-01-11 17:34:46 +0100

    added global variables to cpp part

--- src/lib.cpp
+++ src/lib.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+struct oned_csr_graph;
+
+// global variables from bfs_custom.c
+extern int64_t *column;
+extern int * rowstarts;
+
 void myCppFunction(int n)
 {
     std::string bla;
@@ -16,5 +22,6 @@ void myCppFunction(int n)
 
 void run_bfs_cpp(int64_t root, int64_t* pred)
 {
+    cout << column[0] << endl;
     return;
 }
