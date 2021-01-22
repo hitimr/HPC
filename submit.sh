beb29878a0a0a54d7fce11df878b04f9b03ca4ac
hitimr <hitimr@gmail.com>
2021-01-22 22:53:06 +0100

    updated makefile

--- submit.sh
+++ submit.sh
@@ -7,28 +7,15 @@
 #SBATCH --time=20:00           # job will run for 1 minute (maximum)
 
 
-
-#mpirun ./build/bfs 6 16
-#mpirun ./build/bfs 7 16
-#mpirun ./build/bfs 8 16
-#mpirun ./build/bfs 9 16
-#mpirun ./build/bfs 10 16
-#mpirun ./build/bfs 11 16
-#mpirun ./build/bfs 12 16
-#mpirun ./build/bfs 13 16
-mpirun ./build/bfs 14 16
-#mpirun ./bfs 15 16
-#mpirun ./bfs 16 16
-#mpirun ./bfs 17 16
-#mpirun ./bfs 18 16
-#mpirun ./bfs 19 16
-#mpirun ./bfs 20 16
-#mpirun ./bfs 21 16
-#mpirun ./bfs 22 16
-#mpirun ./bfs 19 16
-#mpirun ./bfs 19 16
-#mpirun ./bfs 19 16
-#mpirun ./bfs 19 16
+cd HPC
+git checkout main
+make clean
+make
+mpirun ./build/bfs 18 16
+git checkout pooling
+make clean
+make
+mpirun ./build/bfs 18 16
 
 
 
